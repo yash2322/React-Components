5a6671faac9d95e3716717a23e4ab8b030414a2e
Yash <useryash2322@gmail.com>
2020-08-16 00:23:03 +0530

    SubmitButton

---
+++ src/components/submitbutton/submitbutton.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './SubmitButtonCSS.css';
+
+class SubmitButton extends React.Component {
+    render() {
+        return (
+            <div className="submitButton">
+                <button className='btn' >
+                    testing button
+                </button>
+            </div>
+        );
+    }
+}
+
+export default SubmitButton;
